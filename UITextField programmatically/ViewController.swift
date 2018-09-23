85c889d3e27bfbb7bbfc5591ef888ac0b522b7ea
maheshprasad <mahesh223prasad@gmail.com>
2018-09-23 17:16:09 +0530

    Initial Commit

---
+++ UITextField programmatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextField programmatically
+//
+//  Created by Mahesh Prasad on 23/09/18.
+//  Copyright © 2018 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
